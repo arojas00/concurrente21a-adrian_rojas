bb9b8c35879db5ca24744aa84b27da92c2fecfea
Adrian Rojas <adrian.rojasarias@ucr.ac.cr>
2021-05-28 12:09:52 -0600

    Add store_numbers method

--- tareas/goldbach_serial/goldbach.c
+++ tareas/goldbach_serial/goldbach.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 /**
  * @brief check if the number si prime
  * @param number the number to be checked
  * @return bool telling if prime number
  */
-_Bool checkPrime(long long number) {
-	_Bool isPrime = true;
+_Bool check_prime(long long number) {
+	_Bool is_prime = true;
 	for (long long i = 2; i <= number / 2; i++) {
 		if (number % i == 0) {
-			isPrime = false;
+			is_prime = false;
 			break;
 		}
 	}
-	return isPrime;
+	return is_prime;
 }
 /**
  * @brief print the two numbers composing the sum
  * @param number the number to chech goldbach
  * @param count the number of sums found
  */
-void printEvenSums(long long number, int count) {
+void print_even_sums(long long number, int count) {
 	FILE *output = stdout;
 	fprintf(output, ": ");
 	int index = 0;
 	for (int i = 2; i < number; i++) {
 		for (int j = i; j < number; j++) {
-			if ((checkPrime(i) == true && checkPrime(j) == true) && i + j == number) {
+			if ((check_prime(i) == true && check_prime(j) == true) && i + j == number) {
 				fprintf(output, "%d + %d", i, j);
 				index++;
 				if (index < count) {
@@ -41,7 +42,7 @@ void printEvenSums(long long number, int count) {
  * @brief find the amount of sums for the even number
  * @param number the number to check goldbach
  */
-void sumEven(long long number) {
+void sum_even(long long number) {
 	FILE *output = stdout;
 	int count = 0;
 	_Bool negative = false;
@@ -52,14 +53,14 @@ void sumEven(long long number) {
 	}
 	for (int i = 2; i < number; i++) {
 		for (int j = i; j < number; j++) {
-			if ((checkPrime(i) == true && checkPrime(j) == true) && i + j == number) {
+			if ((check_prime(i) == true && check_prime(j) == true) && i + j == number) {
 				count++;
 			}
 		}
 	}
 	fprintf(output, "%d sums", count);
 	if (negative == true) {
-		printEvenSums(number, count);
+		print_even_sums(number, count);
 	}
 	else {
 		fprintf(output, "\n");
@@ -70,14 +71,14 @@ void sumEven(long long number) {
  * @param number the number to chech goldbach
  * @param count the number of sums found
  */
-void printOddSums(long long number, int count) {
+void print_odd_sums(long long number, int count) {
 	FILE *output = stdout;
 	fprintf(output, ": ");
 	int index = 0;
 	for (int i = 2; i < number; i++) {
 		for (int j = i; j < number; j++) {
 			for (int k = j; k < number; k++) {
-				if (checkPrime(i) == true && checkPrime(j) == true && checkPrime(k) == true && i + j + k == number) {
+				if (check_prime(i) == true && check_prime(j) == true && check_prime(k) == true && i + j + k == number) {
 					fprintf(output, "%d + %d + %d", i, j, k);
 					index++;
 					if (index < count) {
@@ -93,7 +94,7 @@ void printOddSums(long long number, int count) {
  * @brief find the amount of sums for the  odd number
  * @param number the number to check goldbach
  */
-void sumOdd(long long number) {
+void sum_odd(long long number) {
 	FILE *output = stdout;
 	int count = 0;
 	_Bool negative = false;
@@ -105,7 +106,7 @@ void sumOdd(long long number) {
 	for (int i = 2; i < number; i++) {
 		for (int j = i; j < number; j++) {
 			for (int k = j; k < number; k++) {
-				if (checkPrime(i) == true && checkPrime(j) == true && checkPrime(k) == true && i + j + k == number) {
+				if (check_prime(i) == true && check_prime(j) == true && check_prime(k) == true && i + j + k == number) {
 					count++;
 				}
 			}
@@ -113,7 +114,7 @@ void sumOdd(long long number) {
 	}
 	fprintf(output, "%d sums", count);
 	if (negative == true) {
-		printOddSums(number, count);
+		print_odd_sums(number, count);
 	}
 	else {
 		fprintf(output, "\n");
@@ -123,32 +124,52 @@ void sumOdd(long long number) {
  * @brief check if the number si oven or odd
  * @param number the number to be checked
  */
-void checkEven(long long number) {
+void check_even(long long number) {
 	if (number % 2 == 0) {
-		sumEven(number);
+		sum_even(number);
 	}
 	else {
-		sumOdd(number);
+		sum_odd(number);
 	}
 }
 /**
  * @brief check if the number si valid
  * @param number the number to be checked
  */
-void checkValid(long long number) {
+void check_valid(long long number) {
 	FILE *output = stdout;
 	if (number <= 5 && number >= 0) {
 		fprintf(output, "%lld: NA\n", number);
 	}
 	else {
-		checkEven(number);
+		check_even(number);
 	}
+}
+/**
+ * @brief stores the numbers from the input in an array
+ */
+void store_numbers() {
+	FILE *input = stdin;
+	long long number = 0ll;
+	int size = 10;
+	int index = 0;
+	long long * array = (long long*)calloc(size, sizeof(long long));
+	while (fscanf(input, "%lld", &number) == 1) {
+		array[index] = number;
+		index++;
+		if(index >= size) {
+			size = size + 10;
+			array = realloc(array, size * sizeof(long long));
+		}
+	}
+	for(int i = 0; i < index; i++) {
+		number = array[i];
+		check_valid(number);
+	}
+	free(array);
 }
 
 int main(void) {
-	FILE *input = stdin;
-	long long number = 0ll;
-	while (fscanf(input, "%lld", &number) == 1) {
-		checkValid(number);
-	}
+	store_numbers();
+	return 0;
 }
